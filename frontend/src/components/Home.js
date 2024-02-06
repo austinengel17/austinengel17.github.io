019a5d12cc883d4f727643f2a0c0a331dcd81a24
Austin <austinengel17@gmail.com>
2024-02-05 23:52:39 -0500

    Update about and home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,16 @@
 import Navbar from './Navbar';
+import '../Home.css';
 function Home() {
 
 
     return(
         <>
-            <h1>Welcome to My Personal Site!</h1>
-            <p>Click on one of the links to get started!</p>
+            <h1 class="homepage-heading">Welcome!</h1>
+            <h4 class="homepage-updates">!!!Updates: !!!</h4>
+            <ul class="homepage-update-list">
+                <li>MBTA live subway tracking app in the navbar as "Map" is under construction while in the process of being moved to cloud server</li>
+                <li>Contact page is under construction while in the process of being moved to cloud server **I will not get any messages typed here**</li>
+            </ul>
         </>
     );
 }
